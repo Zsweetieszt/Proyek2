cc4b7fd4dbf4aab9fcdb737111337dc2704b80ed
riyat <ahmadriyadhalmaliki@gmail.com>
2025-03-13 13:33:52 +0700

    update player.c player.h

---
+++ player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <graphics.h>
+#include <conio.h>
+
+#define PIXEL_SIZE 10 // Ukuran setiap kotak pixel
+#define ROWS 26 // Jumlah baris dalam array Mario
+#define COLS 19 // Jumlah kolom dalam array Mario
+
+// Warna dalam graphics.h
+#define BLACK 0
+#define RED COLOR(173, 58, 42)
+#define GREEN COLOR(106, 107, 27)
+#define YELLOW COLOR(227, 157, 37)
+
+// Deklarasi array Mario
+extern int mario[ROWS][COLS];
+
+// Deklarasi fungsi
+void drawCharacter(int x, int y);
+
+#endif // MARIO_H
