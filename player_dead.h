97b609acf6ab030414d22bca24df04be7fa8fe54
riyat <ahmadriyadhalmaliki@gmail.com>
2025-03-13 14:17:02 +0700

    add player dead

--- player_dead.h
+++ player_dead.h
@@ -1,12 +1,12 @@
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef PLAYER_DEAD_H
+#define PLAYER_DEAD_H
 
 #include <graphics.h>
 #include <conio.h>
 
 #define PIXEL_SIZE 3 // Ukuran setiap kotak pixel
-#define ROWS 26 // Jumlah baris dalam array Mario
-#define COLS 19 // Jumlah kolom dalam array Mario
+#define ROWS 16 // Jumlah baris dalam array Mario
+#define COLS 17 // Jumlah kolom dalam array Mario
 
 // Warna dalam graphics.h
 #define BLACK 0
@@ -15,7 +15,7 @@
 #define YELLOW COLOR(227, 157, 37)
 
 // Deklarasi array Mario
-extern int mario[ROWS][COLS];
+extern int mario_dead[ROWS][COLS];
 
 // Deklarasi fungsi
 void drawCharacter(int x, int y);
