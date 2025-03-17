f24e49982bdf076048761fc4a8ab190962a01619
revanramdani31 <permanaevan09@gmail.com>
2025-03-17 13:58:22 +0700

    mengurangi flickering

--- main.c
+++ main.c
@@ -7,18 +7,25 @@ int main() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, (char*)"");  // Inisialisasi mode grafik
 
+    int buffer = 0;
     while (isAlive) {  // Loop utama game
-        cleardevice();  // Bersihkan layar
+        setactivepage(buffer);  // Aktifkan buffer
+        setvisualpage(1 - buffer);  // Tampilkan halaman yang sudah digambar
+
+        cleardevice();  // Bersihkan buffer sebelum menggambar ulang
         //drawBackground();
         drawMap();  // Gambar peta level
         //drawGrid();// Gambar Grid
+      
         drawCharacter(playerX, playerY);  // Gambar pemain
         updateGame();  // Perbarui status game
         handleInput();  // Tangani input pemain
         checkCollisionWithMonster();  // Periksa tabrakan dengan monster
         checkCollisionWithSpike();  // Periksa tabrakan dengan duri
-        displayScore();  // Tambahkan ini agar skor muncul di layar
-        delay(5);  // Delay untuk kecepatan game
+        displayScore();  // Tampilkan skor di layar
+        
+        buffer = 1 - buffer;  // Tukar buffer untuk frame berikutnya
+        delay(20);  // Delay untuk kecepatan game
     }
 
     displayGameOver();  // Tampilkan layar game over
