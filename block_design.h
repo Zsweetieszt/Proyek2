e24fccf837fb37f188ae94cdae0b097d79c72f38
ahmadriyadh123 <ahmadriyadhalmaliki@gmail.com>
2025-03-22 13:17:43 +0700

    adjust file

---
+++ block_design.h
@@ -0,0 +1,9 @@
+#ifndef BLOCK_DESIGN_H
+#define BLOCK_DESIGN_H
+
+void drawRow(int x, int y, int gap, int blockSize);
+void drawRow2(int x, int y, int gap, int blockSize);
+void drawPlatform(int x, int y, int width, int height);
+void drawGround(int x, int y);
+
+#endif
