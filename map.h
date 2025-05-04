a69ad9dbfc47f9f538f07a07f4cf72ce87f04363
ahmadriyadh123 <ahmadriyadhalmaliki@gmail.com>
2025-05-04 09:54:14 +0700

    map files clean code

--- map.h
+++ map.h
@@ -1,29 +1,27 @@
 #ifndef MAP_H
 #define MAP_H
 
+void drawMonsterDebugGrid();
 void drawPlatform(int x, int y);
+void drawGround(int x, int y);
 void drawCoin(int x, int y);
 void drawStar(int x, int y);
 void drawMonster(int x, int y);
-void drawSpike(int x, int y);
-void drawMap();
-void drawGrid();
-void drawGround(int x, int y);
-void drawCloud(int x, int y);
-void drawBackground();
-void drawCloud(int x, int y);
-void drawPipe(int x, int y);
-void drawBrickBlock(int x, int y);
-void drawStoneBlock(int x, int y);
-void drawNextLevel(int x, int y);
-void drawFlag(int x, int y);
-void drawBodyPipe(int x, int y);
-void drawVictoryFlag(int x, int y);
 void animationMonster();
-void drawMonsterDebugGrid();
+void drawSpike(int x, int y);
+void drawCloud(int x, int y);
+void drawGrid();
+void drawStoneBlock(int x, int y);
+void drawBrickBlock(int x, int y);
+void drawCloudBlock(int x, int y)
+void drawBodyPipe(int x, int y);
+void drawPipe(int x, int y);
+void drawBackground();
+void drawNextLevel(int x, int y);
 void renderLevel(GameState gameState);
 void drawNightBackground();
-
-
+void drawFlag(int x, int y);
+void drawVictoryFlag(int x, int y);
+void drawMap();
 
 #endif // MAP_H
